017a8eeb432242dc0d0b68a61a4f307e584e1908
swist <me@swistofon.pl>
2013-11-23 16:19:11 +0000

    adding files to version control

---
+++ public/js/controllers/play.js
@@ -0,0 +1,5 @@
+angular.module('mean.play').controller('PlayController', ['$scope', '$routeParams', '$location', 'Global', 'Play', function ($scope, $routeParams, $location, Global, Play) {
+    $scope.global = Global;
+
+   
+}]);

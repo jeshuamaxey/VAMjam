503a95bce4dd906d4aab4bbf2075c8d4dd95c1f6
swist <me@swistofon.pl>
2013-11-23 18:31:46 +0000

    fabric on playcontroller, accessible within it only!

--- public/js/controllers/play.js
+++ public/js/controllers/play.js
@@ -6,51 +6,48 @@ angular.module('mean.system').controller('PlayController', ['$scope', '$routePar
     	'options' : ['#FDFDFB', '#FFFA8B', '#FFFDD0', '#924F00', '#921800']
     };
 
-    /*
 
-    GOOD LUCK WITH THIS:
 
-    //my own fabric namespace obj
-var f = f || {};
+	$scope.canvas = (function (fabric){
+		var $ = function(el){document.get}
 
-/*********** /
-//http://fabricjs.com/freedrawing/
-/*********** /
+		var f = {
+			init : function() {
+			  
 
-f.init = function() {
-  var $ = function(id){return document.getElementById(id);};
+			  f.canvas = this.__canvas = new fabric.Canvas('shard', {
+			    isDrawingMode: true
+			  });
 
-  f.canvas = this.__canvas = new fabric.Canvas('shard', {
-    isDrawingMode: true
-  });
+			  f.canvas.setHeight(320);
+			  f.canvas.setWidth(320);
+			  f.canvas.on("after:render", function(){f.canvas.calcOffset();});
 
-  f.canvas.setHeight(320);
-  f.canvas.setWidth(320);
-  f.canvas.setBackgroundImage(f.config.imgURL);
-  f.canvas.on("after:render", function(){f.canvas.calcOffset();});
+/*			  fabric.Object.prototype.transparentCorners = false;
 
-  fabric.Object.prototype.transparentCorners = false;
+			  var drawingModeEl = $('drawing-mode'),
+			      drawingOptionsEl = $('drawing-mode-options'),
+			      drawingColorEl = $('drawing-color'),
+			      drawingShadowColorEl = $('drawing-shadow-color'),
+			      drawingLineWidthEl = $('drawing-line-width'),
+			      drawingShadowWidth = $('drawing-shadow-width'),
+			      drawingShadowOffset = $('drawing-shadow-offset'),
+			      clearEl = $('clear-canvas');
 
-  var drawingModeEl = $('drawing-mode'),
-      drawingOptionsEl = $('drawing-mode-options'),
-      drawingColorEl = $('drawing-color'),
-      drawingShadowColorEl = $('drawing-shadow-color'),
-      drawingLineWidthEl = $('drawing-line-width'),
-      drawingShadowWidth = $('drawing-shadow-width'),
-      drawingShadowOffset = $('drawing-shadow-offset'),
-      clearEl = $('clear-canvas');
+			  clearEl.onclick = function() { f.canvas.clear(); };
 
-  clearEl.onclick = function() { f.canvas.clear(); };
+			  $('drawing-mode-selector').onchange = function() {
+			    f.canvas.freeDrawingBrush = new fabric[this.value + 'Brush'](f.canvas);
 
-  $('drawing-mode-selector').onchange = function() {
-    f.canvas.freeDrawingBrush = new fabric[this.value + 'Brush'](f.canvas);
-
-    if (f.canvas.freeDrawingBrush) {
-      f.canvas.freeDrawingBrush.color = drawingColorEl.value;
-      f.canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
-      f.canvas.freeDrawingBrush.shadowBlur = parseInt(drawingShadowWidth.value, 10) || 0;
-    }
-  };
-
-    */
+			    if (f.canvas.freeDrawingBrush) {
+			      f.canvas.freeDrawingBrush.color = drawingColorEl.value;
+			      f.canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
+			      f.canvas.freeDrawingBrush.shadowBlur = parseInt(drawingShadowWidth.value, 10) || 0;
+			    }
+			  };*/
+			}
+		};
+		return f.init()
+	})(window.fabric)
+    
 }]);
